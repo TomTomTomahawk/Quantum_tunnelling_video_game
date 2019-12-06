6ad2bac8ca11eb3ac5870edf888ae0c47d1d5def
TomTomTomahawk <52259295+TomTomTomahawk@users.noreply.github.com>
2019-12-06 19:47:45 +0100

    Add files via upload

--- v2/withphoton.cpp
+++ v2/withphoton.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include <string.h>
+#include <cmath>
 
 using namespace std;
 using namespace sf;
@@ -22,6 +23,12 @@ int main()
     int rayon_photon = 10; // rayon du cercle
 
     double Ec=0;
+    double Ec_max=2;
+
+    double T; // Transmission
+    double m_electron=9.109*pow(10,-31);
+    double h= 6.62607015*pow(10,-34);
+    double h_planck=h/(2*M_PI);
 
     int windowSizeX = 800, windowSizeY = 600;
 
@@ -87,10 +94,64 @@ int main()
     text.setOutlineThickness( 3 );
     text.setPosition(100,50);
 
+//affichage Transmission
+
+    sf::Font font2;
+
+    if ( !font2.loadFromFile( "./Arial.ttf" ) )
+    {
+        std::cout << "Error loading file" << std::endl;
+        
+        system( "pause" );
+    }
+
+    sf::Text text_trans;
+
+    text_trans.setFont( font );
+    
+    text_trans.setString( to_string(Ec) );
+
+    text_trans.setCharacterSize( 25 );
+    
+    text_trans.setFillColor( sf::Color::White );
+    
+    text_trans.setStyle( sf::Text::Style::Bold );
+    
+    text_trans.setOutlineColor( sf::Color::Black );
+    text_trans.setOutlineThickness( 3 );
+    text_trans.setPosition(100,80);
+
+//affichage hauteur du puit
+
+    sf::Font font3;
+
+    if ( !font2.loadFromFile( "./Arial.ttf" ) )
+    {
+        std::cout << "Error loading file" << std::endl;
+        
+        system( "pause" );
+    }
+
+    sf::Text text_puit;
+
+    text_puit.setFont( font );
+    
+    text_puit.setString( to_string(Ec) );
+
+    text_puit.setCharacterSize( 25 );
+    
+    text_puit.setFillColor( sf::Color::White );
+    
+    text_puit.setStyle( sf::Text::Style::Bold );
+    
+    text_puit.setOutlineColor( sf::Color::Black );
+    text_puit.setOutlineThickness( 3 );
+    text_puit.setPosition(100,110);
+
 
 //Affichage puit
 
-    int x1,x2,x3,x4,x5,x6;
+    double x1,x2,x3,x4,x5,x6;
     x1=windowSizeX;
     x2=windowSizeX+150;
     x3=windowSizeX+150;
@@ -98,19 +159,19 @@ int main()
     x5=windowSizeX+300;
     x6=windowSizeX+450;
 
-    int y3, maxpuit, minpuit;
-    maxpuit=600;
-    minpuit=5;
-
-    y3=150;
+    double y3, maxpuit, minpuit, y_puit;
+    maxpuit=2;//hauteur maximal du puits
+    minpuit=0;
+    y_puit=590;//hauteur minimale, c'est a dire pas trop bas
+    y3=(maxpuit - minpuit) *(rand()/(RAND_MAX+1.0));
 
     VertexArray phaut(LinesStrip,6);
-    phaut[0].position = sf::Vector2f(x1, 300);
-    phaut[1].position = sf::Vector2f(x2, 300);
-    phaut[2].position = sf::Vector2f(x3, y3);
-    phaut[3].position = sf::Vector2f(x4, y3);
-    phaut[4].position = sf::Vector2f(x5, 300);
-    phaut[5].position = sf::Vector2f(x6, 300);
+    phaut[0].position = sf::Vector2f(x1, y_puit);
+    phaut[1].position = sf::Vector2f(x2, y_puit);
+    phaut[2].position = sf::Vector2f(x3, windowSizeY-(y3*windowSizeY/maxpuit));
+    phaut[3].position = sf::Vector2f(x4, windowSizeY-(y3*windowSizeY/maxpuit));
+    phaut[4].position = sf::Vector2f(x5, y_puit);
+    phaut[5].position = sf::Vector2f(x6, y_puit);
 
     // on fait tourner le programme tant que la fenêtre n’a pas été fermée
 
@@ -125,7 +186,17 @@ int main()
         window.clear(Color::Black);
 
     if (x_electron >= x1 && x_electron <= x6) {//jeu en periode de puits
-        y_electron=y_electron+1;
+        y_electron=windowSizeY-(Ec*windowSizeY)/Ec_max;
+        
+    if(Ec > y3){
+        T=4*Ec*(Ec-y3)/((4*Ec*(Ec-y3))+y3*y3*sin(sqrt(2*m_electron*(Ec-y3)/h_planck)*((x5-x2)/500000000))*sin(sqrt(2*m_electron*(Ec-y3)/h_planck)*((x5-x2)/500000000))); // Transmission pour E>V0
+    } else {
+    
+        T=4*Ec*(y3-Ec)/((4*Ec*(y3-Ec))+y3*y3*sinh(sqrt(2*m_electron*(y3-Ec)/h_planck)*((x5-x2)/500000000))*sinh(sqrt(2*m_electron*(y3-Ec)/h_planck)*((x5-x2)/500000000))); //Transmission pour E<V0
+
+    }
+    
+        
     }
     else {//jeu comme d'hab
     
@@ -149,10 +220,16 @@ int main()
     }
 
     if (abs((x_photon+rayon_photon) - (x_electron+rayon_electron))<60 && abs((y_photon+rayon_photon) - (y_electron+rayon_electron))<60){
-        Ec=Ec+0.1;
+
+	if (Ec<2){
+        
+	Ec=Ec+0.03;
+	}
         x_photon=x_photon+3;
     } else {
-        Ec = Ec - 0.01;
+	if (Ec>0.003){
+        Ec = Ec - 0.003;
+	}
         x_photon=x_photon+10;
     }
 
@@ -165,7 +242,7 @@ int main()
     x4=windowSizeX+300;
     x5=windowSizeX+300;
     x6=windowSizeX+450;
-    y3=(int)((maxpuit - minpuit) *(rand()/(RAND_MAX+1.0)));
+    y3=(maxpuit - minpuit) *(rand()/(RAND_MAX+1.0));
     }
 
     x1=x1-1;
@@ -175,12 +252,12 @@ int main()
     x5=x5-1;
     x6=x6-1;
         
-    phaut[0].position = sf::Vector2f(x1, 300);
-    phaut[1].position = sf::Vector2f(x2, 300);
-    phaut[2].position = sf::Vector2f(x3, y3);
-    phaut[3].position = sf::Vector2f(x4, y3);
-    phaut[4].position = sf::Vector2f(x5, 300);
-    phaut[5].position = sf::Vector2f(x6, 300);
+    phaut[0].position = sf::Vector2f(x1, y_puit);
+    phaut[1].position = sf::Vector2f(x2, y_puit);
+    phaut[2].position = sf::Vector2f(x3, windowSizeY-(y3*windowSizeY/maxpuit));
+    phaut[3].position = sf::Vector2f(x4, windowSizeY-(y3*windowSizeY/maxpuit));
+    phaut[4].position = sf::Vector2f(x5, y_puit);
+    phaut[5].position = sf::Vector2f(x6, y_puit);
 
     
     //electron.setPosition(x_electron,y_electron);
@@ -190,6 +267,12 @@ int main()
 
     text.setString( "Ec_electron = " + to_string(Ec) + " eV");
     window.draw( text );
+
+    text_trans.setString( "Transmission = " + to_string(T*100.0) + " %");
+    window.draw( text_trans );
+
+    text_puit.setString( "Hauteur du puits = " + to_string(y3) + " eV");
+    window.draw( text_puit );
 
     sp.setPosition(x_electron+5,y_electron+5);
     window.draw(sp);
